b68da86203b590327109bf0871116c428537f9f0
前田紘基 <maedahiroki@maedakoukinoMacBook-Pro.local>
2023-03-29 22:11:09 +0900

    call,applyとthis完了

--- javascript-essence-tutorial-master/050_関数とオブジェクト１/080_call,applyとthis/start/main.js
+++ javascript-essence-tutorial-master/050_関数とオブジェクト１/080_call,applyとthis/start/main.js
@@ -2,6 +2,17 @@ function a(name) {
     console.log('hello ' + name);
 }
 
-const b = a.bind(null, 'Tim');
+const tim = {name: 'Tim'};
+
+const b = a.bind(tim);
 
 b();
+
+a.apply(tim, ['Tim']);
+a.call(tim, 'Tim');
+
+const arry = [1, 2, 3, 4, 5];
+
+//const result = Math.max.apply(null, arry);
+const result = Math.max(...arry);
+console.log(result)
