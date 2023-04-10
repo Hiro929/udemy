883fbd2757735cc97e3cfdaabf0d393b34694252
前田紘基 <maedahiroki@maedakoukinoMacBook-Pro.local>
2023-04-11 00:40:27 +0900

    チェーンメソッドまで完了

--- javascript-essence-tutorial-master/060_関数とオブジェクト２/170_チェーンメソッド/start/main.js
+++ javascript-essence-tutorial-master/060_関数とオブジェクト２/170_チェーンメソッド/start/main.js
@@ -6,14 +6,16 @@ class Person {
 
 	hello(person) {
 		console.log(`${this.name} says hello ${person.name}`);
+		return this;
 	}
 
 	introduce() {
-		console.log(`Hi, I'm ${this.name}, ${this.age} years old.`);
-	}
+		console.log(`Hi, I'm ${this.name}, ${this.age} years old.`);	
+		return this;}
 
 	shakeHands(person) {
 		console.log(`${this.name} shake hands with ${person.name}.`);
+		return this;
 	}
 
 	bye(person) {
@@ -24,7 +26,7 @@ class Person {
 const bob = new Person('Bob', 23);
 const tim = new Person('Tim', 33);
 
-bob.hello(tim);
-bob.introduce();
-bob.shakeHands(tim);
-bob.bye(tim);;
+bob.hello(tim)
+   .introduce()
+   .shakeHands(tim)
+   .bye(tim)
