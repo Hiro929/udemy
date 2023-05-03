26bb0ee59ddd7fd791131409e621c8c8d79b938b
前田紘基 <maedahiroki@maedakoukinoMacBook-Pro.local>
2023-05-03 23:24:03 +0900

    JSONまで完了

--- javascript-essence-tutorial-master/100_様々なオブジェクト/010_Proxy/start/main.js
+++ javascript-essence-tutorial-master/100_様々なオブジェクト/010_Proxy/start/main.js
@@ -1,1 +1,25 @@
 const targetObj = { a: 0 };
+const handler = {
+    set: function(target, prop, value, receiver) {
+        console.log(`[set]: ${prop}`);
+        // target[prop] = value;
+        throw new Error('cannot add prop.');
+    },
+    get: function(target, prop, receiver) {
+        if(target.hasOwnProperty(prop)) {
+            return target[prop];
+        } else {
+            return '-1';
+        }
+        console.log(`[set]: ${prop}`);
+    },
+    deleteProperty: function(target, prop, receiver) {
+        console.log(`[delete]: ${prop}`);
+        delete target[prop]
+    }
+}
+const pxy = new Proxy(targetObj, handler);
+// pxy.a = 1;
+console.log(pxy.b)
+pxy.b;
+delete pxy.a;
