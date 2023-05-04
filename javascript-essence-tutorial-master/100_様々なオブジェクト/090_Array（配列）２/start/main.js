ae7e9d44c30ca0ae21ab5c9975ceac406673efd5
前田紘基 <maedahiroki@maedakoukinoMacBook-Pro.local>
2023-05-04 22:53:26 +0900

    Arrat完了

--- javascript-essence-tutorial-master/100_様々なオブジェクト/090_Array（配列）２/start/main.js
+++ javascript-essence-tutorial-master/100_様々なオブジェクト/090_Array（配列）２/start/main.js
@@ -1,1 +1,18 @@
 const arry = [1, 2, 3, 4, 5];
+
+arry.forEach((v, i, arry) => {
+    console.log(v);
+});
+
+const newArry = arry.map((v, i, arry) => {
+    console.log(v);
+    return v * 2;
+});
+
+// console.log(newArry)
+
+const filterArry = arry.filter((v, i, arry) => {
+    return arry;
+});
+
+console.log(filterArry)
